03c4199e45d0ed0847a82000351cffec43056032
Forrest <zyyu1990@gmail.com>
2022-07-29 09:46:28 +0800

    fix

--- Sources/NearSwift/Signature.swift
+++ Sources/NearSwift/Signature.swift
@@ -6,21 +6,26 @@
 //
 
 import Foundation
+import Base58Swift
 
 public struct Signature {
     public let data: Data
     public let keyType: KeyType
 }
 
+extension Signature: CustomStringConvertible {
+    public var description: String {
+        return "\(keyType.rawValue):\(data.bytes.base58EncodedString)"
+    }
+}
+
 extension Signature: Equatable {
-    
     public static func == (lhs: Self, rhs: Self) -> Bool {
         return lhs.data == rhs.data && lhs.keyType == rhs.keyType
     }
 }
 
 extension Signature: BorshCodable {
-
     public func serialize(to writer: inout Data) throws {
         try keyType.serialize(to: &writer)
         switch keyType {
