9a71644fc4f6d95e85830ab5becc1b95ad40fdea
Forrest <zyyu1990@gmail.com>
2022-11-10 15:23:16 +0800

    Ref

---
+++ Sources/NearSwift/RefFinance/Ref+Token.swift
@@ -0,0 +1,14 @@
+//
+//  RefFiToken.swift
+//  
+//
+//  Created by mathwallet on 2022/11/10.
+//
+
+import Foundation
+
+extension Ref.Token {
+    public func register() {
+        
+    }
+}
---
+++ Sources/NearSwift/RefFinance/Ref.swift
@@ -0,0 +1,21 @@
+//
+//  Ref.swift
+//  
+//
+//  Created by mathwallet on 2022/11/10.
+//
+
+import Foundation
+
+public struct Ref {
+    public static let contract = "v2.ref-finance.near"
+    
+    public struct Storage {
+        public static let PER_TOKEN = "0.00084"
+        public static let TO_REGISTER_WITH_FT = "0.1"
+    }
+    
+    public struct Token {
+        
+    }
+}
